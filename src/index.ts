47649573ac704dce31ce7298199378a1e60497c5
Esteban Miño <efmino@uc.cl>
2024-02-29 15:40:20 -0300

    export messenger references

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { RainbowProvider } from './RainbowProvider';
 export { handleProviderRequest } from './handleProviderRequest';
 export { IMessageSender } from './references/messengers';
 export { AddEthereumChainProposedChain } from './references/chains';
+export { RequestArguments, RequestResponse } from './references/messengers';

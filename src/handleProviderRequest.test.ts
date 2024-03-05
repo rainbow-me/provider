9d19bb135258e495e304911c3f05be0d4a93039c
Esteban Miño <efmino@uc.cl>
2024-03-05 13:06:52 -0300

    sign methods

--- src/handleProviderRequest.test.ts
+++ src/handleProviderRequest.test.ts
@@ -5,9 +5,16 @@ import {
   ProviderRequestPayload,
   RequestResponse,
 } from './references/messengers';
-import { Address, isHex } from 'viem';
+import { Address, isAddress, isHex } from 'viem';
 import { mainnet, optimism } from 'viem/chains';
 import { StaticJsonRpcProvider } from '@ethersproject/providers';
+import { verifyMessage } from '@ethersproject/wallet';
+import {
+  MessageTypes,
+  SignTypedDataVersion,
+  TypedMessage,
+  recoverTypedSignature,
+} from '@metamask/eth-sig-util';
 
 const TESTMAR27_ETH_ADDRESS: Address =
   '0x5e087b61aad29559e31565079fcdabe384b44614';
@@ -15,6 +22,40 @@ const RAINBOWWALLET_ETH_ADDRESS: Address =
   '0x7a3d05c70581bd345fe117c06e45f9669205384f';
 const RAINBOWWALLET_ETH_TX_HASH =
   '0xfc621a4577ba3398adc0800400b2ba2c408ab76cdc1521dadbfc802dc93a8b37';
+const TX_HASH =
+  '0x43cfbb52ec99192e96f34a42b37354cfabd6845403e9473e921030da9751d12d';
+const SIGN_SIGNATURE = '0x123456789'
+
+const TYPED_MESSAGE = {
+  domain: {
+    chainId: 1,
+    name: 'Ether Mail',
+    verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+    version: '1',
+  },
+  types: {
+    Mail: [
+      { name: 'from', type: 'Person' },
+      { name: 'to', type: 'Person' },
+      { name: 'contents', type: 'string' },
+    ],
+    Person: [
+      { name: 'name', type: 'string' },
+      { name: 'wallet', type: 'address' },
+    ],
+  },
+  value: {
+    contents: 'Hello, Bob!',
+    from: {
+      name: 'Cow',
+      wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+    },
+    to: {
+      name: 'Bob',
+      wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+    },
+  },
+};
 
 describe('handleProviderRequest', () => {
   const getFeatureFlagsMock = vi.fn(() => ({ custom_rpc: true }));
@@ -51,7 +92,40 @@ describe('handleProviderRequest', () => {
         return new StaticJsonRpcProvider('http://127.0.0.1:8545');
     }
   });
-  const messengerProviderRequestMock = vi.fn(() => Promise.resolve({}));
+  const messengerProviderRequestMock = vi.fn((payload) => {
+    // This is just passing messages so we can test the responses
+    switch (payload.method) {
+      case 'eth_requestAccounts': {
+        return Promise.resolve(RAINBOWWALLET_ETH_ADDRESS);
+      }
+      case 'eth_sendTransaction': {
+        return Promise.resolve(TX_HASH);
+      }
+      case 'personal_sign': {
+        return Promise.resolve(
+          SIGN_SIGNATURE + 'personal_sign' + payload.params[0],
+        );
+      }
+      case 'eth_signTypedData': {
+        return Promise.resolve(
+          SIGN_SIGNATURE + 'eth_signTypedData' + payload.params[0],
+        );
+      }
+      case 'eth_signTypedData_v3': {
+        return Promise.resolve(
+          SIGN_SIGNATURE + 'eth_signTypedData_v3' + payload.params[0],
+        );
+      }
+      case 'eth_signTypedData_v4': {
+        return Promise.resolve(
+          SIGN_SIGNATURE + 'eth_signTypedData_v4' + payload.params[0],
+        );
+      }
+      default: {
+        return Promise.resolve({});
+      }
+    }
+  });
   const onAddEthereumChainMock = vi.fn(() => ({ chainAlreadyAdded: true }));
   const onSwitchEthereumChainNotSupportedMock = vi.fn(() => null);
   const onSwitchEthereumChainSupportedMock = vi.fn(() => null);
@@ -283,5 +357,109 @@ describe('handleProviderRequest', () => {
       { id: 1 },
     );
     expect(isHex(response.result)).toBeTruthy();
+  });
+
+  it('should call eth_requestAccounts correctly', async () => {
+    const response = await transport.send(
+      {
+        id: 1,
+        method: 'eth_requestAccounts',
+        params: [],
+        meta: {
+          sender: { url: 'https://dapp1.com' },
+          topic: 'providerRequest',
+        },
+      },
+      { id: 1 },
+    );
+    expect(response.result[0]).toBe(RAINBOWWALLET_ETH_ADDRESS);
+  });
+
+  it('should call eth_sendTransaction correctly', async () => {
+    const response = await transport.send(
+      {
+        id: 1,
+        method: 'eth_sendTransaction',
+        params: [
+          {
+            from: '0x5B570F0F8E2a29B7bCBbfC000f9C7b78D45b7C35',
+            gas: '0x5208',
+            to: '0x5B570F0F8E2a29B7bCBbfC000f9C7b78D45b7C35',
+            value: '0x9184e72a000',
+          },
+        ],
+        meta: {
+          sender: { url: 'https://dapp1.com' },
+          topic: 'providerRequest',
+        },
+      },
+      { id: 1 },
+    );
+    expect(response.result).toBe(TX_HASH);
+  });
+
+  it('should call personal_sign correctly', async () => {
+    const response = await transport.send(
+      {
+        id: 1,
+        method: 'personal_sign',
+        params: ['personal_sign_message'],
+        meta: {
+          sender: { url: 'https://dapp1.com' },
+          topic: 'providerRequest',
+        },
+      },
+      { id: 1 },
+    );
+    expect(response.result).toBe(SIGN_SIGNATURE + 'personal_sign'+ 'personal_sign_message');
+  });
+
+  it('should call eth_signTypedData correctly', async () => {
+    const response = await transport.send(
+      {
+        id: 1,
+        method: 'eth_signTypedData',
+        params: ['eth_signTypedData_message'],
+        meta: {
+          sender: { url: 'https://dapp1.com' },
+          topic: 'providerRequest',
+        },
+      },
+      { id: 1 },
+    );
+    expect(response.result).toBe(SIGN_SIGNATURE + 'eth_signTypedData'+ 'eth_signTypedData_message');
+  });
+
+  it('should call eth_signTypedData_v3 correctly', async () => {
+    const response = await transport.send(
+      {
+        id: 1,
+        method: 'eth_signTypedData_v3',
+        params: ['eth_signTypedData_v3_message'],
+        meta: {
+          sender: { url: 'https://dapp1.com' },
+          topic: 'providerRequest',
+        },
+      },
+      { id: 1 },
+    );
+    expect(response.result).toBe(SIGN_SIGNATURE + 'eth_signTypedData_v3'+ 'eth_signTypedData_v3_message');
+  });
+
+  it('should call eth_signTypedData_v4 correctly', async () => {
+    const response = await transport.send(
+      {
+        id: 1,
+        method: 'eth_signTypedData_v4',
+        params: [TYPED_MESSAGE],
+        meta: {
+          sender: { url: 'https://dapp1.com' },
+          topic: 'providerRequest',
+        },
+      },
+      { id: 1 },
+    );
+    console.log(response.result)
+    expect(response.result).toBe(SIGN_SIGNATURE + 'eth_signTypedData_v4'+ TYPED_MESSAGE);
   });
 });

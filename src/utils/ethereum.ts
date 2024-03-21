586aa46965b41173bd071d7ca2bdccf70ef10b0c
Bruno Barbieri <1247834+brunobar79@users.noreply.github.com>
2024-03-21 12:13:39 -0400

    FIx for react native (#12)

--- src/utils/ethereum.ts
+++ src/utils/ethereum.ts
@@ -4,7 +4,7 @@ export const normalizeTransactionResponsePayload = (
   payload: TransactionResponse,
 ): TransactionResponse => {
   // Firefox can't serialize functions
-  if (navigator.userAgent.toLowerCase().includes('firefox')) {
+  if (navigator.userAgent?.toLowerCase().includes('firefox')) {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { wait: _, ...cleanedPayload } = payload;
     return cleanedPayload as TransactionResponse;
